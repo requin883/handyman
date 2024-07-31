5addf9e383e05ecb9e338ea5928f3dc837af5636
Javier <requin883@gmail.com>
2024-07-31 05:55:05 -0700

    fix: proptypes

--- handyman/src/components/ServiceCard.jsx
+++ handyman/src/components/ServiceCard.jsx
@@ -1,9 +1,15 @@
+import PropTypes from 'prop-types';
+
 export const ServiceCard = ({ title, img }) => {
   return (<div className="grid gap-2 max-w-lg place-items-center">
-    <img src={img} alt="" />
+    {img && <img src={img} alt="" />}
     <div className="grid grid-flow-col gap-2 place-items-center">
       <p className="font-bold">{title}</p>
       <button className="font-bold border-2 p-2 border-yellow-400 rounded-lg">Get a quote</button>
     </div>
   </div>)
 }
+ServiceCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  img: PropTypes.string
+};
